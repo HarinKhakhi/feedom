b4af372c1116d973a11c2860693c60f2f0df4e3b
Jeet Hirakani <51905899+jeeth25@users.noreply.github.com>
2024-06-23 04:10:40 -0700

    fix tag selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
-import SearchAppBar from './components/SearchAppBar.jsx';
-import VideoPlayer from './components/VideoPlayer.jsx';
-import TagSelector from './components/TagSelector.jsx';
-import { Container, Typography } from '@mui/material';
-
+import React from "react";
+import SearchAppBar from "./components/SearchAppBar.jsx";
+import VideoPlayer from "./components/VideoPlayer.jsx";
+import TagSelector from "./components/TagSelector.jsx";
+import { Container, Typography } from "@mui/material";
 
 const App = () => {
-  
-  const tags = ['React', 'JavaScript', 'CSS', 'HTML', 'Node.js', 'Express', 'MongoDB'];
+  const tags = [
+    "React",
+    "JavaScript",
+    "CSS",
+    "HTML",
+    "Node.js",
+    "Express",
+    "MongoDB",
+  ];
   return (
-    
     <div>
       <div>
-     <SearchAppBar />
-     <VideoPlayer />
-     </div>
-  
-    <div>
-    <Typography variant="h4" component="h1" gutterBottom>
-      Tag Selector
-    </Typography>
-    <TagSelector tags={tags} />
+        <SearchAppBar />
+      </div>
+      <div>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Tag Selector
+        </Typography>
+        <TagSelector tags={tags} />
+      </div>
+      <div>
+        <VideoPlayer />,
+      </div>
     </div>
-    </div>
-    
   );
-
 };
 
 export default App;
